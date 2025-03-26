b46e56723f0f1b97fe07bfb59ee81d3474b5d6a0
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2025-03-26 22:07:41 +0100

    cleanup

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-from .views import views
-
-def create_app() -> Flask:
-    app = Flask(__name__)
-    app.register_blueprint(views)
-
-    return app
--- app/static/js/map.js
+++
@@ -1,6 +0,0 @@
-
-const map = L.map('map').setView([43.950001, 4.81667], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
